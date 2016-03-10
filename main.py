67a17c0ca4c759ca29c1b31142aabe6bc195f4ac
selesdepselesnul <selesdepselesnul@gmail.com>
2016-03-11 06:39:15 +0700

    give title line edit placeholder text

--- main.py
+++ main.py
@@ -15,12 +15,24 @@ import wikipedia
 form_class = uic.loadUiType('copaspedia.ui')[0]
 
 class MyWindowClass(QWidget, form_class):
+
+
     def __init__(self, parent=None):
         QWidget.__init__(self, parent)
         self.setupUi(self)
         self.title_line_edit.returnPressed.connect(self.handle_title_pressed)
-        for lang in wikipedia.languages():
+        self.page_mode_combo_box.activated.connect(self.handle_mode_selected)
+        self.page_mode_combo_box.addItems(['Page', 'Summary'])
+        self.filter_combo_box.addItems(
+            ['Content', 'Images', 'Links', 'References', 'Section'])
+        for lang in sorted(wikipedia.languages()):
             self.lang_combo_box.addItem(lang)
+
+    def handle_mode_selected(self, mode):
+        if mode == 0:
+            self.title_line_edit.setPlaceholderText('Title')
+        else:
+            self.title_line_edit.setPlaceholderText('Query')
 
     def handle_title_pressed(self):
         title = self.title_line_edit.text()
