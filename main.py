6ff57bafe19135ec8b7dbccd082449688ebf6d7b
selesdepselesnul <selesdepselesnul@gmail.com>
2016-03-10 18:41:18 +0700

    ready to kick in

--- main.py
+++ main.py
@@ -7,5 +7,20 @@ website: http://selesdepselesnul.com
 github : https://github.com/selesdepselesnul
 """
 
-import wikipedia
-print(wikipedia.search('indo'))
+from PyQt5 import uic
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
+
+form_class = uic.loadUiType('copaspedia.ui')[0]
+
+class MyWindowClass(QWidget, form_class):
+    def __init__(self, parent=None):
+        QWidget.__init__(self, parent)
+        self.setupUi(self)
+
+
+
+app = QApplication(sys.argv)
+myWindow = MyWindowClass(None)
+myWindow.show()
+app.exec_()
