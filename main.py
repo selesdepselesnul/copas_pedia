f9960c173c3d05b01adb27c220b78bdb3f031969
selesdepselesnul <selesdepselesnul@gmail.com>
2016-03-11 07:35:27 +0700

    refactor

--- main.py
+++ main.py
@@ -16,34 +16,26 @@ form_class = uic.loadUiType('copaspedia.ui')[0]
 
 class MainWindowController(QWidget, form_class):
 
-    PAGE = 0
-
     def __init__(self, parent=None):
         QWidget.__init__(self, parent)
         self.setupUi(self)
         self.title_line_edit.returnPressed.connect(self.handle_title_pressed)
-        self.page_mode_combo_box.activated.connect(self.handle_mode_selected)
-        self.page_mode_combo_box.addItems(['Page', 'Summary'])
-        self.filter_combo_box.addItems(
-            ['Content', 'Images', 'Links', 'References', 'Section'])
+        self.page_combo_box.addItems(
+            ['Content', 'Images', 'Links', 'References', 'Summary'])
         for lang in sorted(wikipedia.languages()):
             self.lang_combo_box.addItem(lang)
-
-    def handle_mode_selected(self, mode):
-        if mode == MainWindowController.PAGE:
-            self.title_line_edit.setPlaceholderText('Title')
-        else:
-            self.title_line_edit.setPlaceholderText('Query')
 
     def handle_title_pressed(self):
         title = self.title_line_edit.text()
         if title:
             wikipedia.set_lang(self.lang_combo_box.currentText())
             wiki = wikipedia.page(title=title)
-            print(self.lang_combo_box.currentText())
-            # wikipedia.set_lang(self.lang_combo_box.currentText())
-            if self.filter_combo_box.currentText() == 'Content':
+            page = self.page_combo_box.currentText()
+            if page == 'Content':
                 self.content_text_edit.setPlainText(wiki.content)
+            elif page == 'Images':
+                self.content_text_edit.setPlainText(wiki.images.pop())
+
         else:
             print('empty')
 
